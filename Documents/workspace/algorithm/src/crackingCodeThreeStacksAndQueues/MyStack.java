2326a571edadddb00efa545cbd43a21293d7991e
Kimmy <Kimmy@DESKTOP-NLH3M3K>
2016-11-15 08:45:37 -0800

    Mystack

--- Documents/workspace/algorithm/src/crackingCodeThreeStacksAndQueues/MyStack.java
+++ Documents/workspace/algorithm/src/crackingCodeThreeStacksAndQueues/MyStack.java
@@ -1,5 +1,50 @@
 package crackingCodeThreeStacksAndQueues;
 
-public class MyStack {
+import java.util.EmptyStackException;
 
+public class MyStack<T>{
+
+	public static class StackNode<T> 
+	{
+		
+		private T data ;
+		private StackNode<T> next ;
+		private StackNode(T data){
+			this.data = data ;	
+		}
+			
+	}
+	
+	private StackNode<T> top ;
+	
+	public T pop()
+	{
+		if (top == null) throw new EmptyStackException();
+		//StackNode<T> temp = new StackNode<T>(top.data);
+		T temp = top.data ;
+		top = top.next ;
+		return temp ;
+	}
+ 	
+	public void push(T item)
+	{
+		if (top == null) top.data = item ;
+		
+		StackNode<T> current = new StackNode<T>(item);
+		current.next = top ;
+		top = current ;
+	}
+	
+	public T peek(){
+		if (top == null) throw new EmptyStackException();
+		return top.data ;
+	
+	}
+	
+	public boolean isEmpty(){
+		
+		return top == null ;
+		
+	}	
+	
 }
