9aafe0cf1d450ec2a64ab7f08f0140c34192d4a7
Kimmy <Kimmy@DESKTOP-NLH3M3K>
2017-02-21 22:29:05 -0800

    1

--- Documents/workspace/algorithm/src/crackingCodeFourTreesAndGraphs/route_Between_Node.java
+++ Documents/workspace/algorithm/src/crackingCodeFourTreesAndGraphs/route_Between_Node.java
@@ -1,5 +1,39 @@
 package crackingCodeFourTreesAndGraphs;
 
-public class route_Between_Node {
+import java.util.LinkedList;
 
+import javax.xml.soap.Node;
+
+public class route_Between_Node {
+// BFS 
+	enum State { unvisited ,visited ,visiting; }
+	boolean search (Graph g , Node start , Node end){
+		if (start == end) return true ;//checNodee 1 node case
+		LinkedList<Node> q = new Linkedlist<Node>();//treat with queue
+		for (Node u : g.getNodes()){
+			u.state = State.unvisited;
+		}
+		start.state = State.visiting;
+		q.add(start);
+		Node u ;
+		while (!q.isEmpty()){
+			u = q.removeFirst();
+			if (u!= null){
+			   for(Node v : u.getAdjacent){
+				   if(v.state == State.visited){
+					   if (v == end){
+						   return true ;
+					   } else {
+						   v.state = State.visiting ;
+						   q.add(v);
+					   }
+				   }
+					   
+			   }
+			   u.state = State.visited ;	   
+		     }	
+	     }
+		return false ;				
+     }
 }
+
