8ba35f5a6fc22381ef1d3593e346c6444308df75
Kimmy <Kimmy@DESKTOP-NLH3M3K>
2017-03-27 23:49:42 -0700

    min tree

--- Documents/workspace/algorithm/src/crackingCodeFourTreesAndGraphs/Minimal_Tree.java
+++ Documents/workspace/algorithm/src/crackingCodeFourTreesAndGraphs/Minimal_Tree.java
@@ -1,5 +1,21 @@
 package crackingCodeFourTreesAndGraphs;
 
 public class Minimal_Tree {
+	
+	
+	TreeNode Min_tree(int arry[]){
+		return Min_tree(arry , 0 , arry.length - 1 ) ;		
+	}
+	
+	TreeNode Min_tree(int Arry[], int start ,int end){
+		if (end < start) return null ;
+		int mid = (start + end)/2;
+		TreeNode n = new TreeNode (Arry[mid]);
+		n.left = Min_tree (Arry, start , mid-1 );
+		n.right = Min_tree (Arry, mid , end ); 
+		return n ;
+		
+		
+	}
 
 }
